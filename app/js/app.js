ea5bbee1a06ec12978fceb819de03a15dede5ad9
Lucas Guilherme Ramos Piovesan <lucasguilhermerpiovesan@gmail.com>
2022-07-14 20:16:22 -0300

    task-0007:fix: Fix bug for github url path

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,7 @@ const $ = document.querySelector.bind(document);
 
 $('.base-header').innerHTML = Header_Controller.display_header();
 
-if (window.location.pathname == "/" || window.location.pathname == "/index.html") {    
+if (window.location.pathname == "/" || window.location.pathname == "/index.html" || window.location.pathname == "/CinemaJS/") {    
     $('.home').innerHTML = Highlight_Controller.display_highlights();
 }
 
