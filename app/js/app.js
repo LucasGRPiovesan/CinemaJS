f07e294499fa61ec1534b8f92cf589fe3b4f37f0
Lucas <lucasguilhermem@outlook.com>
2022-08-05 23:06:35 -0300

    task-0010:feat: Responsive layout for menu and user page

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,20 @@ const $ = document.querySelector.bind(document);
 $('.base-header').innerHTML += Header_Controller.display_header();
 $('.base-footer').innerHTML = Footer_Controller.display_footer();
 
+$('.principal__mobile-header--menu').addEventListener('click', ()=> {
+    $('.base-header').classList.add('display-menu');
+    $('.principal').style.display = 'none';
+    $('.base-footer').style.display = 'none';
+});
+
+$('.base-header__mobile--icon').addEventListener('click', ()=> {
+    $('.base-header').classList.toggle('display-menu');
+    setTimeout(()=> {
+        $('.principal').style.display = 'flex';
+        $('.base-footer').style.display = 'flex';
+    },200);
+});
+
 if (FUNCTIONS.verify_index_url(window.location.pathname)) $('.home').innerHTML = Highlight_Controller.display_highlights();
 
 document.querySelectorAll('.test').forEach(area => area.innerHTML = Movie_Controller.display_movie());
